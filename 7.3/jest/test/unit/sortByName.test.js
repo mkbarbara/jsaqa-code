c6cf7a22c62658ea3ff64276645e5b0f45ac3c9a
mkbarbara <makarena2001@yandex.ru>
2024-02-07 11:19:39 +0300

    7.3 done

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -14,4 +14,26 @@ describe("Books names test suit", () => {
       "Гарри Поттер",
     ]);
   });
+
+  it("Sorting with one item should return the same item", () => {
+    expect(sorting.sortByName(["Alice"])).toEqual(["Alice"]);
+  });
+
+  it("Books names should be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName([
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+        "Гарри Поттер",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+  
+  it("Sorting with one item should return the same item", () => {
+    expect(sorting.sortByName(["Alice", "Alice"])).toEqual(["Alice", "Alice"]);
+  });
 });
